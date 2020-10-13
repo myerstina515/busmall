ce58a656bf95673aca23bf32345ddc55821e69fc
TINA MYERS <myers.tina515@gmail.com>
2020-10-13 16:54:52 -0700

    added the chart and adjusted some styling

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ new Products('img/pen.jpg', 'pen');
 new Products('img/pet-sweep.jpg', 'pet-sweep');
 new Products('img/scissors.jpg', 'scissors');
 new Products('img/shark.jpg', 'shark');
-new Products('img/pet-sweep.jpg', 'pet-sweep');
+new Products('img/sweep.png', 'sweep');
 new Products('img/tauntaun.jpg', 'tauntaun');
 new Products('img/unicorn.jpg', 'unicorn');
 new Products('img/usb.gif', 'usb');
@@ -74,11 +74,17 @@ function render(imageElement){
   imageElement.src = allProducts[randomIndex].filepath;
   imageElement.alt = allProducts[randomIndex].name;
 
-  if (recentRandomNumbers.length > 2){
-    recentRandomNumbers = [];
-  }
-  recentRandomNumbers.push(randomIndex);
+  allProducts[randomIndex].views++;
 
+  recentRandomNumbers.push(randomIndex);
+  console.log(recentRandomNumbers);
+  if (recentRandomNumbers.length > 5){
+    recentRandomNumbers.shift();
+    recentRandomNumbers.shift();
+    recentRandomNumbers.shift();
+    console.log('shifted array', recentRandomNumbers);
+  }
+  // This was from class demo, but seems to not do anything to help
 }
 
 // helper functions
@@ -104,10 +110,10 @@ productOptionsElement.addEventListener('click', function(event){
       if (chosenProduct === allProducts[i].name){
         allProducts[i].votes++;
       }
-      render(imageOneElement);
-      render(imageTwoElement);
-      render(imageThreeElement);
     }
+    render(imageOneElement);
+    render(imageTwoElement);
+    render(imageThreeElement);
     votingRoundTracker++;
   } else {
     finalTally();
@@ -124,13 +130,110 @@ var resultsList = document.getElementById('resultsSection');
 // // First, I need to add each time an image is chosen.
 
 
-// I CAN'T GET THIS TO WORK!!!!!!!!!!!! :(
+
 function finalTally(){
   for (var i = 0; i < allProducts.length - 1; i++){
     var imageOne = document.createElement('li');
-    imageOne.textContent = `${allProducts[i].name} received ${allProducts[i].votes} votes over 25 rounds of voting`;
+    imageOne.textContent = `${allProducts[i].name} received ${allProducts[i].votes} votes and ${allProducts[i].views} views.`;
     resultsList.appendChild(imageOne);
   }
+  for (var j = 0; j < allProducts.length; j++){
+    myChart.data.datasets[0].data.push(allProducts[j].votes);
+  }
+
+  for (var k = 0; k < allProducts.length; k++){
+    myChart.data.labels.push(allProducts[k].name);
+    console.log(myChart.data.labels[k] + 'is speaking');
+  }
+
+  for (var x = 0; x < allProducts.length; x++){
+    myChart.data.datasets[1].data.push(allProducts[x].views);
+  }
+  myChart.update();
 }
 
 
+
+// starting the chart
+
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+
+    // I need the labels array to populate with the allProducts.name array.
+    // I should create a variable for labels that loops over the allProducts array
+    // Then I need the data array to populate with the allProducts.vote array. The "data" array should loop over the .vote array just like the previous one did over the .name array
+    labels: [],
+    datasets: [{
+      label: '# of Votes',
+      data: [],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(20, 20, 131, 0.2)',
+        'rgba(0, 200, 130, 0.2)',
+        'rgba(200, 145, 165, 0.2)',
+        'rgba(245, 204, 21, 0.2)',
+        'rgba(110, 45, 230, 0.2)',
+        'rgba(2, 235, 133, 0.2)',
+        'rgba(234, 34, 154, 0.2)',
+        'rgba(53. 235, 75, 0.2)',
+        'rgba(143, 165, 65, 0.2)',
+        'rgba(255, 93, 138, 0.2)',
+        'rgba(78, 86, 123, 0.2)',
+        'rgba(135, 246, 78, 0.2)',
+        'rgba(245, 67, 48, 0.2)',
+        'rgba(25, 236, 145, 0.2'
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+        'rgba(20, 20, 131, 1)',
+        'rgba(0, 200, 130, 1)',
+        'rgba(200, 145, 165, 1)',
+        'rgba(245, 204, 21, 1)',
+        'rgba(110, 45, 230, 1)',
+        'rgba(2, 235, 133, 1)',
+        'rgba(234, 34, 154, 1)',
+        'rgba(53. 235, 75, 1)',
+        'rgba(143, 165, 65, 1)',
+        'rgba(255, 93, 138, 1)',
+        'rgba(78, 86, 123, 1)',
+        'rgba(135, 246, 78, 1)',
+        'rgba(245, 67, 48, 1)',
+        'rgba(25, 236, 145, 1)'
+      ],
+      borderWidth: 1
+    }, {
+      label: '# of Views',
+      data: [],
+      borderColor: 'rgba(0, 0, 0, 1)'
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    },
+    layout: {
+      padding: {
+        left: 20,
+        right: 40,
+        bottom: 20,
+        top: 50,
+      }
+    }
+  }
+});
